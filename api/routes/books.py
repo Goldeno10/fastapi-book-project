8fa79f6ac77487f5d73077a8a8fba8ea679ce640
Goldeno10 <ibrahimmuhammad271@gmail.com>
2025-02-13 19:34:18 +0100

    fix/ get book by id excetption response

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,6 +1,6 @@
 from typing import OrderedDict
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from fastapi.responses import JSONResponse
 
 from api.db.schemas import Book, Genre, InMemoryDB
@@ -53,11 +53,10 @@ async def get_book_by_id(book_id: int) -> Book:
     """ Get book by ID """
     book = db.get_book(book_id)
     if not book:
-        return JSONResponse(
+        raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            content={"detail": "Book not found"}
-        )
-    
+            detail="Book not found"
+        )  
     return JSONResponse(
         status_code=status.HTTP_200_OK,
         content=book.model_dump()
